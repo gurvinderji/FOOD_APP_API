593c95a4cc3c50ca8fa8ac1bcc35dfc9c42f55d7
gurvinderji <gurvinderji2000@gmail.com>
2025-11-05 07:58:53 +0530

    category schema

---
+++ models/category.model.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "category title is required"],
+    },
+    imageUrl: {
+      type: String,
+      default:
+        "https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png",
+    },
+  },
+  { timestamps: true }
+);
+
+const CategoryModel = model("Category", categorySchema);
+
+export default CategoryModel;
